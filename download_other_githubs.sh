a0d33484fdb728e85ce53d86a4a8b554e153f224
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-07 17:22:16 +0200

    Add pics and scripts [skip ci]

--- download_other_githubs.sh
+++ download_other_githubs.sh
@@ -2,14 +2,6 @@
 
 cd ..
 
-if [ ! -d RibiClasses ]; then
- git clone https://github.com/richelbilderbeek/RibiClasses
-fi
-
-if [ ! -d RibiLibraries ]; then
- git clone https://github.com/richelbilderbeek/RibiLibraries
-fi
-
-if [ ! -d Triangle ]; then
- git clone https://github.com/richelbilderbeek/Triangle
-fi
+git clone https://github.com/richelbilderbeek/RibiClasses
+git clone https://github.com/richelbilderbeek/RibiLibraries
+git clone https://github.com/richelbilderbeek/Triangle
