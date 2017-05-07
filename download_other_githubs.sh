6d8830144a128bca02b9e49bfe1f922252e5e981
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-07 17:27:32 +0200

    Download plane GitHb

--- download_other_githubs.sh
+++ download_other_githubs.sh
@@ -2,6 +2,7 @@
 
 cd ..
 
+git clone https://github.com/richelbilderbeek/plane
 git clone https://github.com/richelbilderbeek/RibiClasses
 git clone https://github.com/richelbilderbeek/RibiLibraries
 git clone https://github.com/richelbilderbeek/Triangle
